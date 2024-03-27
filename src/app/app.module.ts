a1d17df44d2ccd34544bcc11bbc4b34a316ce17c
Neti <98841356+Netidion@users.noreply.github.com>
2024-03-27 20:35:57 +0200

    Adding basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {HeaderComponent} from "./header/header.component";
+import {RecipesComponent} from "./recipes/recipes.component";
+import {RecipeListComponent} from "./recipes/recipe-list/recipe-list.component";
+import {RecipeDetailsComponent} from "./recipes/recipe-details/recipe-details.component";
+import {RecipeItemComponent} from "./recipes/recipe-list/recipe-item/recipe-item.component";
+import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
+import {ShoppingEditComponent} from "./shopping-list/shopping-edit/shopping-edit.component";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailsComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.css'
+})
+export class RecipeListComponent {
+
+}
