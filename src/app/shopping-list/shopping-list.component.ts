92bbde0b065cadc3e90cbd72cc90235e7b98f9f3
Neti <98841356+Netidion@users.noreply.github.com>
2024-03-30 17:37:37 +0200

    Adding ingredient in shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,23 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {Ingredient} from "../models/ingredient.model";
 
 @Component({
   selector: "shopping-list",
   templateUrl:"./shopping-list.component.html",
   styleUrl:"./shopping-list.component.css"
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient("Eggs", 2),
+    new Ingredient("Milk", 1),
+    new Ingredient("Onions", 1),
+    new Ingredient("Zucchini", 1),
+    new Ingredient("Corn", 1),
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
